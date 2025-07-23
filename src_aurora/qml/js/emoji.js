227709c173842c32baeee2dfeab5daa51d84846e
Nikolay Sinyov <nikolay.sinyov@gmail.com>
2025-07-23 10:11:23 +0300

    - Подправил определение символов. Спасибо нейронке, подсказала, что забыли)

--- src_aurora/qml/js/emoji.js
+++ src_aurora/qml/js/emoji.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2025 Tatiana <0728th61@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
 .pragma library  // Remove this if you want per-instance functions
 
 function getCodePoint(chars) {
@@ -17,10 +36,21 @@ function convertToOriginalHtml(textWithEmojis) {
 
     while ((match = emojiRegex.exec(textWithEmojis)) !== null) {
         var charS = match[0];
+        var codePoint;
 
-        if (charS.length === 2 || getCodePoint(charS) > 0xFFFF) {
-            var codePoint = getCodePoint(charS).toString(16);
-            htmlParts.push('<img src="file:///usr/share/ru.neochapay.maximus/qml/emojiSvgs/' + codePoint + '.svg" width="40" height="40"/>');
+        if (charS.length === 1) {
+            codePoint = charS.charCodeAt(0);
+        } else {
+            codePoint = getCodePoint(charS);
+        }
+
+        if (charS.length === 2 ||
+            codePoint > 0xFFFF ||
+            (codePoint >= 0x2600 && codePoint <= 0x26FF) ||  // Miscellaneous Symbols
+            (codePoint >= 0x2700 && codePoint <= 0x27BF)     // Dingbats
+        ) {
+            var hexCode = codePoint.toString(16);
+            htmlParts.push('<img src="file:///usr/share/ru.neochapay.maximus/qml/emojiSvgs/' + hexCode + '.svg" width="40" height="40"/>');
             htmlParts.push(' ');
         } else {
             htmlParts.push(charS);
